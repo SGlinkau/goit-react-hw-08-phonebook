700ed084d4c96a209a92f59950e014da504d8c98
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2023-12-05 09:34:38 +0100

    Update index.js
    Fixed issue with routing that caused main page to be blanked

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ import { store, persistor } from './redux/store';
 import 'modern-normalize';
 import './styles.css';
 
+const basename = '/goit-react-hw-08-phonebook';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename={basename}>
           <App />
         </BrowserRouter>
       </PersistGate>
